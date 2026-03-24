17945122884cb532c02aa3269211ce51fb3462e6
Gabriel Ribeiro de Menezes <gabriel.r.menezes@aluno.senai.br>
2026-03-24 08:25:49 -0300

    Refactor server.js to improve structure and clarity

--- server.js
+++ server.js
@@ -148,6 +148,4 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+module.exports = app;
